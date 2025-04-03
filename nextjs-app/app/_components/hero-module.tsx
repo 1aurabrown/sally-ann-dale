77bfe79e13d9263e35522b6bf4820c52a8bb2b88
Laura Brown <laura+git@laurabrown.studio>
2025-04-03 16:49:16 -0400

    fix hero and logo alignment

--- nextjs-app/app/_components/hero-module.tsx
+++ nextjs-app/app/_components/hero-module.tsx
@@ -21,7 +21,7 @@ export default function HeroModule({
   return (
     <section className="page-padding">
       {image && (
-        <Image className="w-full mx-auto max-w-sm sm:max-w-md md:max-w-xl lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl" image={image} />
+        <Image className="w-full px-10 md:px-25 mx-auto max-w-4xl" image={image} />
       )}
       <div className="first:mt-[17vw] first:lg:mt-[30vh] md:flex md:gap-16 space-y-10 md:space-y-0 mt-12">
         {heading?.length && (
